ced739c2d0175d0b13e64cb38f0e0c95a7efc4aa
ddcruzp <david.delacruz9041@gmail.com>
2020-12-10 19:58:09 -0500

    Anadiendo mejoras

---
+++ helloworld.cs
@@ -0,0 +1,12 @@
+using System; 
+
+namespace Prueba
+{
+    class Program
+    {   
+        Console.WriteLine("Hello World");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("Hello"+nombe+".Bienvenido");
+    }
+    
+}
