96e3b92b366dc150dbd32752991945a0a226a19d
ddcruzp <david.delacruz9041@gmail.com>
2020-12-11 12:55:44 -0500

    mejorando el saludo y midiendo el tiempo de tecleado

--- helloworld.cs
+++ helloworld.cs
@@ -1,14 +1,19 @@
 using System; 
+using System.Diagnostics;
 
 namespace Prueba
 {
     class Program
     {   
-        static void Main (string [] argv) 
+        static void Main (string [] args) 
         {
-            Console.WriteLine("Hello World");
+            StopWatch.crono = new StopWatch();
+            Console.WriteLine("Hello, write your name pleace:");
+            crono.Start();
             string nombre = Console.ReadLine();
-            Console.WriteLine("Hello " +nombre +".Bienvenido");
+            crono.stop();
+            Console.WriteLine("Hello" + nombre + "wellcome");
+            Console.WriteLine("te has demorado en teclear tu nombre" + crono.Elapsedmilliseconds + "ms.");
         }
     }
 }
