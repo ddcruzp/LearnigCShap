51a63671b686c3a9f99757407c43b47f30ea074c
ddcruzp <david.delacruz9041@gmail.com>
2020-12-15 23:02:37 -0500

    cambios en el fichero distancia

--- distancia.cs
+++ distancia.cs
@@ -1,5 +1,6 @@
 using System; 
-using Math;
+
+
 
 namespace Distancia
 {
@@ -7,17 +8,18 @@ namespace Distancia
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("\nEntre la coordenada x del primer punto : ");
-            double x1 = double.Parse(Console.ReadLine())
+            double x1 = double.Parse(Console.ReadLine());
             Console.WriteLine("\nEntre la coordenada y del primer punto : ");
-            double y1 = double.Parse(Console.ReadLine())
+            double y1 = double.Parse(Console.ReadLine());
 
             Console.WriteLine("\nEntre la coordenada x del segundo punto : ");
-            double x2 = double.Parse(Console.ReadLine())
+            double x2 = double.Parse(Console.ReadLine());
             Console.WriteLine("\nEntre la coordenada y del segundo punto : ");
-            double y2 = double.Parse(Console.ReadLine())
+            double y2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("La distancia entre los dospuntos es {0}",Math.sqrt(Math.Pow(x2 - x1 , 2) + Math.Pow(y2 - y1 , 2)));
+            Console.WriteLine("La distancia entre los dospuntos es {0}",Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
         }
     }
 }
