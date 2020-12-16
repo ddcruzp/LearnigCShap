ea86a245b9d2a75b96df23331ee2740bd60a0fd7
ddcruzp <david.delacruz9041@gmail.com>
2020-12-15 23:12:18 -0500

    guardando cambios en dotnet

--- distancia.cs
+++ distancia.cs
@@ -1,25 +1,25 @@
-using System; 
+// using System; 
 
 
 
-namespace Distancia
-{
-    class program
-    {
-        static void Main(string[] args)
-        {
+// namespace Distancia
+// {
+//     class program
+//     {
+//         static void Main(string[] args)
+//         {
             
-            Console.WriteLine("\nEntre la coordenada x del primer punto : ");
-            double x1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("\nEntre la coordenada y del primer punto : ");
-            double y1 = double.Parse(Console.ReadLine());
+//             Console.WriteLine("\nEntre la coordenada x del primer punto : ");
+//             double x1 = double.Parse(Console.ReadLine());
+//             Console.WriteLine("\nEntre la coordenada y del primer punto : ");
+//             double y1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nEntre la coordenada x del segundo punto : ");
-            double x2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("\nEntre la coordenada y del segundo punto : ");
-            double y2 = double.Parse(Console.ReadLine());
+//             Console.WriteLine("\nEntre la coordenada x del segundo punto : ");
+//             double x2 = double.Parse(Console.ReadLine());
+//             Console.WriteLine("\nEntre la coordenada y del segundo punto : ");
+//             double y2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("La distancia entre los dospuntos es {0}",Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
-        }
-    }
-}
+//             Console.WriteLine("La distancia entre los dospuntos es {0}",Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+//         }
+//     }
+// }
--- helloworld.cs
+++ helloworld.cs
@@ -1,19 +1,19 @@
-using System; 
-using System.Diagnostics;
+// using System; 
+// using System.Diagnostics;
 
-namespace Prueba
-{
-    class Program
-    {   
-        static void Main (string [] args) 
-        {
-            Stopwatch crono = new Stopwatch();
-            Console.WriteLine("Hello, write your name pleace:");
-            crono.Start();
-            string nombre = Console.ReadLine();
-            crono.Stop();
-            Console.WriteLine("Hello" + nombre + "wellcome");
-            Console.WriteLine("te has demorado en teclear tu nombre" + crono.ElapsedMilliseconds + "ms.");
-        }
-    }
-}
+// namespace Prueba
+// {
+//     class Program
+//     {   
+//         static void Main (string [] args) 
+//         {
+//             Stopwatch crono = new Stopwatch();
+//             Console.WriteLine("Hello, write your name pleace:");
+//             crono.Start();
+//             string nombre = Console.ReadLine();
+//             crono.Stop();
+//             Console.WriteLine("Hello" + nombre + "wellcome");
+//             Console.WriteLine("te has demorado en teclear tu nombre" + crono.ElapsedMilliseconds + "ms.");
+//         }
+//     }
+// }
