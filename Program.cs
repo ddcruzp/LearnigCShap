4ef4e2f7fc7e487f0c1d0a2b72b28aed625495c0
ddcruzp <david.delacruz9041@gmail.com>
2021-01-12 15:06:16 -0500

    Adicionandolo todo para subirlo al repositorio remoto.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
-﻿// using System;
+﻿using System;
 
-// namespace LearnigCShap
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//              Console.WriteLine("\nEntre la coordenada x del primer punto : ");
-//             double x1 = double.Parse(Console.ReadLine());
-//             Console.WriteLine("\nEntre la coordenada y del primer punto : ");
-//             double y1 = double.Parse(Console.ReadLine());
-
-//             Console.WriteLine("\nEntre la coordenada x del segundo punto : ");
-//             double x2 = double.Parse(Console.ReadLine());
-//             Console.WriteLine("\nEntre la coordenada y del segundo punto : ");
-//             double y2 = double.Parse(Console.ReadLine());
-
-//             Console.WriteLine("La distancia entre los dospuntos es {0}",Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
-//         }
-//     }
-// }
+namespace LearnigCShap
+{
+    class Program
+    {
+        static void pero(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
